743f4f087dcea767871c530a7aeb57262346f279
Nick Crews <nicholas.b.crews@gmail.com>
2021-05-17 18:07:14 -0600

    SerialScale: Change to `serial.threaded` utils
    There are built in helper classes that can do exactly what we were
    doing, in 1/2 as much code. The old code didn't even work,
    we read stuff but it wouldn't parse. We also don't need a totally
    separate helper process, we can get away with a lightweight Thread
    instead.

--- boa/scale/serial.py
+++ boa/scale/serial.py
@@ -1,188 +1,86 @@
-import atexit
 import logging
-import multiprocessing
+import queue
 import time
 
 import serial
+import serial.threaded
 from serial.tools.list_ports import comports
 
 import scale
 
 
 class SerialScale(scale.Scale):
-    """A scale which is connected via USB serial cable"""
-
-    MAX_BUFFERED_READINGS = 10000
+    """A scale accessible via a serial port."""
 
     def __init__(self, port, baudrate=9600):
+        # Create (but don't open yet) our Serial port instance
+        self.serial = serial.Serial()
+        self.serial.port = port
+        self.serial.baudrate = baudrate
 
-        self.port = port
-        self._baudrate = baudrate
+        self._open = False
+        self._readingsQ = queue.Queue()
 
-        self.readingsQ = multiprocessing.Queue(self.MAX_BUFFERED_READINGS)
-        self.commandQ = multiprocessing.Queue()
-        self.reader = SerialReader(port, baudrate, self.readingsQ, self.commandQ)
+    class Reader(serial.threaded.ReaderThread):
+        def __init__(self, scale):
+            self.serial = scale.serial
 
-    def __repr__(self):
-        status = "open" if self.is_open() else "closed"
-        return (
-            status
-            + " Serial Scale at port "
-            + self.port
-            + " with baudrate "
-            + str(self.baudrate)
-        )
+            class Handler(serial.threaded.LineReader):
+                def __init__(self, scale):
+                    super().__init__()
+                    self.scale = scale
+
+                def handle_line(self, line: str):
+                    try:
+                        value = int(line.strip())
+                    except ValueError as e:
+                        logging.error(e)
+                        return
+                    reading = (time.time(), value)
+                    self.scale._readingsQ.put(reading)
+
+                def connection_lost(self, exc):
+                    if exc:
+                        logging.error(exc)
+
+            def handler_factory():
+                return Handler(scale)
+
+            super().__init__(self.serial, handler_factory)
+
+        def run(self):
+            self.serial.open()
+            super().run()
 
     def open(self):
+        logging.debug("Opening %s", self)
+        self.reader = type(self).Reader(self)
         self.reader.start()
 
     def __str__(self):
-        return "Serial Scale at " + self.port
+        return f"Serial Scale at {self.serial.port}"
 
     def is_open(self):
         return self.reader.is_alive()
 
     def close(self):
-        # poison pill
-        self.commandQ.put(None)
-
-    @property
-    def baudrate(self):
-        return self._baudrate
-
-    @baudrate.setter
-    def baudrate(self, newval):
-        self._baudrate = newval
-        self.commandQ.put({"attr": "baudrate", "val": newval})
+        logging.debug("Closing %s", self)
+        self.reader.close()
 
     def read(self):
         readings = []
-        while not self.readingsQ.empty():
-            readings.append(self.readingsQ.get())
+        while not self._readingsQ.empty():
+            readings.append(self._readingsQ.get())
         return readings
-
-
-class SerialReader(multiprocessing.Process):
-    """Used by SerialScale to read from the scale smoothly in a different process"""
-
-    # in seconds
-    LINK_TIMEOUT = 5
-    READ_TIMEOUT = 1
-
-    MAX_PACKET_SIZE = 20
-
-    def __init__(self, portname, baudrate, readingsQ, commandQ):
-        super().__init__()
-        self.daemon = True
-
-        self.portname = portname
-        self._baudrate = baudrate
-        self.readingsQ = readingsQ
-        self.commandQ = commandQ
-
-        self._ser = None
-        atexit.register(self.close)
-
-    def run(self):
-        self._open_port()
-        self._wait_for_link()
-        last = time.time()
-        while self._ser.is_open:
-            # check for updates from outside this thread
-            if not self.commandQ.empty():
-                cmd = self.commandQ.get()
-                if cmd is None:
-                    # poison pill, exit this process
-                    break
-                else:
-                    setattr(self, cmd["attr"], cmd["val"])
-            # read a line or timeout and return empty string
-            line = self._readline()
-            if line:
-                try:
-                    reading = int(line)
-                except:
-                    # must have had trouble parsing. probably the baudrate is wrong, but we can ignore it
-                    continue
-
-                # Throw out the oldeast reading if the Q is full
-                while self.readingsQ.full():
-                    self.readingsQ.get()
-                pair = (time.time(), reading)
-                self.readingsQ.put(pair)
-            else:
-                # we didn't read anything, must have timeout out
-                logging.error("didn't read anything")
-                break
-        self.close()
-
-    def _readline(self):
-        chars = []
-        for i in range(self.MAX_PACKET_SIZE):
-            try:
-                c = self._ser.read(1)
-            except serial.SerialException as e:
-                # probably some I/O problem such as disconnected USB serial
-                return None
-            if not c:
-                # timed out on read of individual byte
-                return None
-            chars.append(c)
-            # check last two characters
-            if chars[-2:] == [b"\r", b"\n"]:
-                # looks promising...
-                try:
-                    strs = [c.decode() for c in chars[:-2]]
-                    result = "".join(strs)
-                    return result
-                except:
-                    # problem parsing, must be some other problem
-                    return "error"
-            else:
-                # got end of line or there's a problem with baudrate so we never get eol
-                return None
-
-    def _open_port(self):
-        # try to open the serial port
-        try:
-            self._ser = serial.Serial(self.portname, baudrate=self.baudrate)
-        except serial.SerialException as e:
-            raise e
-        except ValueError as e:
-            raise e
-        if not self._ser.is_open:
-            raise serial.SerialException(
-                "couldn't open the port {port_name}".format(port_name=self._ser.name)
-            )
-        self._ser.timeout = self.READ_TIMEOUT
-        logging.info("successfully opened serial port %s", self.portname)
-
-    def _wait_for_link(self):
-        """The Arduino reboots when it initiates a USB serial connection, so wait for it to resume streaming readings"""
-        start_time = time.time()
-        while True:
-            time.sleep(0.1)
-            if self._ser.in_waiting:
-                # We got a reading! Hurray!
-                break
-            if time.time() - start_time > self.LINK_TIMEOUT:
-                # Something went wrong the arduino took too long
-                self._ser.close()
-                break
-
-    def close(self):
-        if self._ser:
-            self._ser.close()
 
     @property
     def baudrate(self):
-        return self._baudrate
+        return self.serial.baudrate
 
     @baudrate.setter
     def baudrate(self, newval):
-        self._baudrate = newval
-        if self._ser:
-            self._ser.baudrate = newval
+        logging.debug("%s baudrate set to %s", self, newval)
+        self.serial.baudrate = newval
 
 
 class SerialScaleSearcher(scale.ScaleSearcher):
@@ -196,13 +94,13 @@ class SerialScaleSearcher(scale.ScaleSearcher):
     @classmethod
     def available_scales(cls):
         available_ports = set(port.device for port in comports())
-        used_ports = set(scale.port for scale in cls.used_scales)
+        used_ports = set(scale.serial.port for scale in cls.used_scales)
 
         to_remove = used_ports - available_ports
         to_add = available_ports - used_ports
 
         cls.used_scales = [
-            scale for scale in cls.used_scales if scale.port not in to_remove
+            scale for scale in cls.used_scales if scale.serial.port not in to_remove
         ]
         cls.used_scales += [SerialScale(port) for port in to_add]
 
